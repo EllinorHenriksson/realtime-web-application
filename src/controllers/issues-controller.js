8a6ab9dc69444c2bf8199ad89497fde72e35a610
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-07-12 19:53:10 +0200

    Display form for updating issue

--- src/controllers/issues-controller.js
+++ src/controllers/issues-controller.js
@@ -24,6 +24,7 @@ export class IssuesController {
       const viewData = {
         issues: data.map(issue => {
           return {
+            id: issue.iid,
             author: issue.author.avatar_url,
             title: issue.title,
             description: issue.description
@@ -32,6 +33,47 @@ export class IssuesController {
       }
 
       res.render('issues/index', { viewData })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Returns a HTML form for updating a task.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async update (req, res, next) {
+    try {
+      const response = await fetch(`${process.env.GITLAB_API}/issues/${req.params.id}`, {
+        headers: {
+          'Private-Token': process.env.GITLAB_SECRET
+        }
+      })
+
+      if (!response.ok) {
+        if (response.status === 404) {
+          req.session.flash = { type: 'danger', text: 'Someone deleted the issue you wanted to update.' }
+          res.redirect('..')
+        } else {
+          throw new Error('Failed to load the requested resource from GitLab API.')
+        }
+      }
+
+      const data = await response.json()
+
+      const viewData = {
+        id: data.iid,
+        title: data.title,
+        description: data.description,
+        state: data.state
+      }
+
+      console.log(viewData)
+
+      res.render('issues/update', { viewData })
     } catch (error) {
       next(error)
     }
--- src/routes/issues-router.js
+++ src/routes/issues-router.js
@@ -11,3 +11,5 @@ const controller = new IssuesController()
 
 // OBS! Lägg till routes
 router.get('/', (req, res, next) => controller.index(req, res, next))
+
+router.get('/:id/update', (req, res, next) => controller.update(req, res, next))
