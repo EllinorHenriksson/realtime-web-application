810c7dba4bd3d8d853429e5a58b43961c1a52def
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-07-27 18:45:26 +0200

    Remove commented-out code for issue list

--- public/js/index.js
+++ public/js/index.js
@@ -16,36 +16,6 @@ if (issueTemplate) {
  * @param {object} issue - The updated issue.
  */
 function updateIssueTable (issue) {
-  /*
-  const issueList = document.querySelector('#issue-list')
-  const issueItem = document.getElementById(issue.id)
-
-  if (issue.state === 'opened') {
-    if (issueItem) {
-      // Update item if it already exists.
-      issueItem.querySelector('.title').innerText = issue.title
-      issueItem.querySelector('.description').firstChild.innerText = issue.description
-    } else {
-      // Add item if it does not exist.
-      const issueNode = issueTemplate.content.cloneNode(true)
-      issueNode.querySelector('div').setAttribute('id', issue.id)
-      const url = issue.author.avatar_url ? issue.author.avatar_url : './img/profile.png'
-      issueNode.querySelector('img').setAttribute('src', url)
-      issueNode.querySelector('img').setAttribute('title', issue.author.name)
-      issueNode.querySelector('.issue-id').innerText = `#${issue.id}`
-      issueNode.querySelector('.title').innerText = issue.title
-      issueNode.querySelector('.description').firstChild.innerText = issue.description
-      issueNode.querySelector('a').setAttribute('href', `./issues/${issue.id}/update`)
-
-      issueList.appendChild(issueNode)
-    }
-  } else if (issue.state === 'closed') {
-    if (issueItem) {
-      // Delete item if the issue has been closed.
-      issueItem.remove()
-    }
-  }
-  */
   const tableBody = document.querySelector('tbody')
   const tableRow = document.getElementById(issue.id)
 
