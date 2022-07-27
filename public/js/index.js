b50325262a8c2e58cd25352dc1f5fcc5ce3e9aa4
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-07-27 15:35:40 +0200

    Modify index.js to work with table html element

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,7 @@ if (issueTemplate) {
  * @param {object} issue - The task to add.
  */
 function updateIssueList (issue) {
+  /*
   const issueList = document.querySelector('#issue-list')
   const issueItem = document.getElementById(issue.id)
 
@@ -42,6 +43,35 @@ function updateIssueList (issue) {
     if (issueItem) {
       // Delete item if the issue has been closed.
       issueItem.remove()
+    }
+  }
+  */
+  const tableBody = document.querySelector('tbody')
+  const issueRow = document.getElementById(issue.id)
+
+  if (issue.state === 'opened') {
+    if (issueRow) {
+      // Update row if it already exists.
+      issueRow.querySelector('.title').innerText = issue.title
+      issueRow.querySelector('.description').firstChild.innerText = issue.description
+    } else {
+      // Add row if it does not exist.
+      const issueNode = issueTemplate.content.cloneNode(true)
+      issueNode.querySelector('tr').setAttribute('id', issue.id)
+      const url = issue.author.avatar_url ? issue.author.avatar_url : './img/profile.png'
+      issueNode.querySelector('.author img').setAttribute('src', url)
+      issueNode.querySelector('.author img').setAttribute('title', issue.author.name)
+      issueNode.querySelector('.issue-id').innerText = `#${issue.id}`
+      issueNode.querySelector('.title').innerText = issue.title
+      issueNode.querySelector('.description').firstChild.innerText = issue.description
+      issueNode.querySelector('a').setAttribute('href', `./issues/${issue.id}/update`)
+
+      tableBody.appendChild(issueNode)
+    }
+  } else if (issue.state === 'closed') {
+    if (issueRow) {
+      // Delete row if the issue has been closed.
+      issueRow.remove()
     }
   }
 }
